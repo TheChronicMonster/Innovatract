252187e981fdf19ef516d43d60ddff9302a54617
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-23 11:34:08 -0400

    added stream fetching and video player display

--- client/src/components/Livepeer.js
+++ client/src/components/Livepeer.js
@@ -7,6 +7,7 @@ const Livepeer = () => {
     const livepeerObject = new Livepeer(apiKey);
     const [data, setData] = useState(null);
     const [streamUrl, setStreamUrl] = useState(null);
+    const [showButton, setShowButton] = useState(false);
 
     const content = {
         "name": "test_stream", 
@@ -40,13 +41,36 @@ const Livepeer = () => {
         livepeerObject.Stream.create(content).then((res) => {
             console.log(res);
             setData(res);
+            setShowButton(true);
         });
+    };
+
+    const getStreamUrl = async () => {
+        const url = `https://livepeer.com/api/session?limit=20&parentId=${data.id}`;
+
+        const listOfAllStreams = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        });
+
+        if (listOfAllStreams.data.length === 0) {
+            alert("No stream detected");
+            return;
+        }
+
+        setStreamUrl(listOfAllStreams.data[0].mp4Url);
+        console.log(listOfAllStreams.data[0].mp4Url);
+
+        if (streamUrl === "") alert("stream is currently processing");
     };
 
     return (
         <div>
             <button onClick={startStream}>Record Goal</button>
-            {data ? <p>stream key: {data.streamKey}</p> : null}
+            {data ? <p>stream key: {data.streamKey} (plug into streaming sofrware)</p> : null}
+            {showButton ? <button>Play Stream</button> : null}
+            {streamUrl ? <ShakaPlayer src={streamUrl} /> : null}
         </div>
     );
 }

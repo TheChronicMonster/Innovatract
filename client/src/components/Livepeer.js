b23e71168618c6bed4663cbefc3c04bc25f327c2
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-09-23 10:18:56 -0400

    added livepeer functionality to function

--- client/src/components/Livepeer.js
+++ client/src/components/Livepeer.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Livepeer = () => {
+    const Livepeer = require("livepeer-nodejs");
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const livepeerObject = new Livepeer(apiKey);
+    const [data, setData] = useState([]);
+    const [streamUrl, setStreamUrl] = useState(null);
+
+    const content = {
+        "name": "test_stream", 
+        "profiles": [
+            {
+                "name": "720p",
+                "bitrate": 2000000,
+                "fps": 30,
+                "width": 1280,
+                "height": 720
+            },
+            {
+                "name": "480p",
+                "bitrate": 1000000,
+                "fps": 30,
+                "width": 854,
+                "height": 480
+            },
+            {
+                "name": "360p",
+                "bitrate": 500000,
+                "fps": 30,
+                "width": 640,
+                "height": 360
+            },
+        ],
+        "record": true
+    };
+    
+    const startStream = () => {
+        livepeerObject.Stream.create(content).then((res) => {
+            setData(res);
+        });
+    };
+
+    useEffect(() => {
+        startStream();
+    }, []);
+
     return (
         <div>
             <button>Record Goal</button>

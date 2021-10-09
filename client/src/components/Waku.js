3c590bb3da99cd9e0af1f92d434e30b509d79009
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-09 11:25:39 -0400

    created several functions for viewing and sending messages using waku services

--- client/src/components/Waku.js
+++ client/src/components/Waku.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Waku, WakuMessage } from 'js-waku';
 
-const Waku = () => {
+const WakuComponent = () => {
+    const waku = await Waku.create({ bootstrap: true });
+
+    waku.relay.addObserver((msg) => {
+        console.log("Message received:", msg.payloadAsUtf8)
+    }, ["/my-cool-app/1/my-use-case/proto"]);
+
+    const sendMessage = async (rawMessage) => {
+        const msg = await WakuMessage.fromUtf8String(rawMessage, "/my-cool-app/1/my-use-case/proto");
+        await waku.relay.send(msg);
+    }
+
+    const viewMessages = async () => {
+        const messages = await waku.store.queryHistory(['/my-cool-app/1/my-use-case/proto']);
+        messages.forEach((msg) => {
+            console.log('Message retrieved:', msg.payloadAsUtf8);
+        });
+    }
+
     return (
         <div>
-            
+            <h1>Waku</h1>
         </div>
     )
 }
 
-export default Waku;
+export default WakuComponent;
